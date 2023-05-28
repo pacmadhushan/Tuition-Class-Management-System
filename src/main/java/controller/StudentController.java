4d512f7eba6de612ed3cd248d700ada45ddc07df
pacmadhushan <chathuramadhushanpa@gmail.com>
2023-05-29 00:00:25 +0530

    second commit

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import dto.StudentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.StudentService;
+
+@RestController
+@RequestMapping("api/v1/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+    @PostMapping(value = "/saveStudent")
+    public ResponseEntity saveStudent(@RequestBody StudentDTO studentDTO){
+        try {
+            String res = studentService.saveStudent(studentDTO);
+            if(res.equals("00")){
+
+            } else if (res.equals("06")) {
+
+            }else {
+
+            }
+
+        }catch (){
+
+        }
+
+    }
+
 }
--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -1,4 +1,33 @@
 package service;
 
+import dto.StudentDTO;
+import entity.Student;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import repo.StudentRepo;
+import util.VarList;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class StudentService {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+     @Autowired
+     private ModelMapper modelMapper;
+    public String saveStudent(StudentDTO studentDTO){
+        if(studentRepo.existsById(studentDTO.getStdID())){
+            return VarList.RSP_DUPLICATED;
+        }else {
+            studentRepo.save(modelMapper.map(studentDTO, Student.class));
+            return VarList.RSP_SUCCESS;
+
+        }
+
+    }
 }
